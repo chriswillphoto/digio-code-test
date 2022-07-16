5b37dfb28605e829a47796d4f16f985c3b30306b
Christopher Nekich <christophernekich@gmail.com>
2022-07-16 19:50:02 +1000

    main

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
-function sayMyName(name: string): void {
-  if (name === "Heisenberg") {
-    console.log("You're right 👍");
-    console.log(process.argv)
-  } else {
-    console.log("You're wrong 👎");
-  }
-}
+import LogReader from "./LogReader";
+import { pipe } from "ts-functional-pipe";
+import IpAddressHandler from "./IpAddressHandler";
+import UrlHandler from "./UrlHandler";
+import analysisHandler from "./AnalysisHandler";
 
-sayMyName("Heisenberg");
+const main = () => {
+  const pathArg = process.argv[2];
+
+  if (!pathArg) throw "Error: Must use file path as argument";
+
+  const pipeline = pipe(LogReader, IpAddressHandler, UrlHandler, analysisHandler);
+
+  return pipeline(pathArg);
+};
+
+if (require.main === module) {
+  main();
+}
